85a74814f767c0bd92af4281640bc2ff08d5c440
Annam Raza <annam.raza@my.jcu.edu.au>
2022-06-18 02:04:34 +1000

    running bowtie2 code as test

--- hpc/testscripts/samtoolstest/03_bowtie2.sh
+++ hpc/testscripts/samtoolstest/03_bowtie2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -j oe
 #PBS -m ae
-#PBS -N trimtest
+#PBS -N maptest
 #PBS -M FIRSTNAME.LASTNAME@jcu.edu.au
 #PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=1:mem=4gb
+#PBS -l select=1:ncpus=1:mem=10gb
 
 cd $PBS_O_WORKDIR
 shopt -s expand_aliases
@@ -12,9 +12,9 @@ source /etc/profile.d/modules.sh
 echo "Job identifier is $PBS_JOBID"
 echo "Working directory is $PBS_O_WORKDIR"
 
-module load trimmomatic
-module load java
+module load bowtie2
 
-java -jar trimmomatic-0.39.jar SE 77_1_C5H1BACXX_GATCAG_L001_R1.fastq.gz 77_1_C5H1BACXX_GATCAG_L001_R1_trim.fastq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10
+bowtie2-build haliotis_genome.fa haliotis_genome.btindex
 
-java -jar trimmomatic-0.39.jar SE 88_1_C5H1BACXX_AGTCAA_L001_R1.fastq.gz 88_1_C5H1BACXX_AGTCAA_L001_R1_trim.fastq.gz ILLUMINACLIP:TruSeq3-SE:2:30:10
+bowtie2 -x haliotis_genome.btindex -U 77_1_C5H1BACXX_GATCAG_L001_R1_trim.fastq.gz -S 77_1_maptest.sam
+bowtie2 -x haliotis_genome.btindex -U 88_1_C5H1BACXX_AGTCAA_L001_R1_trim.fastq.gz -S 88_1_maptest.sam
