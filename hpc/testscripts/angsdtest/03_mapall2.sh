23eea9c4ae6246f591b938673988fb6ea41bbf51
Annam Raza <annam.raza@my.jcu.edu.au>
2022-06-27 17:36:22 +1000

    separated indexing and mapping scripts

---
+++ hpc/testscripts/angsdtest/03_mapall2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -m ae
+#PBS -N mapangsdtest
+#PBS -M FIRSTNAME.LASTNAME@jcu.edu.au
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=200gb
+
+cd $PBS_O_WORKDIR
+shopt -s expand_aliases
+source /etc/profile.d/modules.sh
+echo "Job identifier is $PBS_JOBID"
+echo "Working directory is $PBS_O_WORKDIR"
+
+module load bowtie2
+
+bowtie2 -x hailotis_gen -U mapinput.txt -S mapped
