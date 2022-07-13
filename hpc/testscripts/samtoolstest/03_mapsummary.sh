92bfbb1397ce7269e05372bfc25cd8842f545b87
Annam Raza <annam.raza@my.jcu.edu.au>
2022-07-14 01:06:24 +1000

    mapping rates summary attempt 1

---
+++ hpc/testscripts/samtoolstest/03_mapsummary.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -m ae
+#PBS -N mapsummarytest
+#PBS -M FIRSTNAME.LASTNAME@jcu.edu.au
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=200gb
+
+cd $PBS_O_WORKDIR
+shopt -s expand_aliases
+source /etc/profile.d/modules.sh
+echo "Job identifier is $PBS_JOBID"
+echo "Working directory is $PBS_O_WORKDIR"
+
+module load samtools
+
+dt=$(date)
+
+summarise_mapping(){
+  input=$1
+  fs=$(samtools flagstat $input)
+  fname=${input%_merged.bam}
+  echo $fname $fs
+}
+
+echo $dt
+
+for f in *_merged.bam;do
+  input=$f
+
+
+parallel -j 20 summarise_mapping ::: *_merged.bam > 03_summary.log
+
+cat 03_summary.log | awk 'OFS="\t"{print $1,$37}' | sed 's/(//' | sed 's/%//' > mapping_rates.tsv
+
+echo $dt
