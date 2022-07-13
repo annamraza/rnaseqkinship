127858fc785ee0c6cd9e36ea5f0563f061902769
Annam Raza <annam.raza@my.jcu.edu.au>
2022-07-13 18:26:37 +1000

    primus test 1

---
+++ hpc/testscripts/samtoolstest/06_primus.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -m ae
+#PBS -N pedigreetest2
+#PBS -M FIRSTNAME.LASTNAME@jcu.edu.au
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=1:mem=100gb
+
+cd $PBS_O_WORKDIR
+shopt -s expand_aliases
+source /etc/profile.d/modules.sh
+echo "Job identifier is $PBS_JOBID"
+echo "Working directory is $PBS_O_WORKDIR"
+
+module load singularity
+
+singularity run /sw/containers/primus-1.9.0.sif run_PRIMUS.pl plink.genome
